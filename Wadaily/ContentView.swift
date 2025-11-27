164ad859d72a28c5637b4b58f6ec7a982de96fe8
urassh <shuto.1011@icloud.com>
2025-11-27 22:42:18 +0900

    first commit

---
+++ Wadaily/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wadaily
+//
+//  Created by 浦山秀斗 on 2025/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wadaily/WadailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WadailyApp.swift
+//  Wadaily
+//
+//  Created by 浦山秀斗 on 2025/11/27.
+//
+
+import SwiftUI
+
+@main
+struct WadailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
